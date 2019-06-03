6494255ad73e1503becbd2ce1d1f259b585d9956
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-03 13:54:32 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,7 +23,7 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
         name: "Lord Capulet",
-an :age of "50".
+        age: "50".
         },
       :matriarch => {
         #your key/value pairs here
