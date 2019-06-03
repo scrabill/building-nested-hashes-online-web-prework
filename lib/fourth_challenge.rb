7a6706dd0e2c41c417ed10cfab1f86bd16839cc0
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-03 13:59:55 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,8 +6,13 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "woried"
+        },
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -18,6 +23,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
