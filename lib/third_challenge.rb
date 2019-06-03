8e5a0d95690d9ca73e19587120fd07e996a723eb
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-03 13:54:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,6 +27,9 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Capulet",
+        age: "51".
+
         },
       :heroine => {
         #your key/value pairs here
